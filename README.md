eaf2ffa172a9836107436de0d959e07eaa6f200d
Sam Sepiol <94198807+sam-sepiol1@users.noreply.github.com>
2024-10-07 15:14:30 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,58 @@
+# Task Manager CLI Application
+
+This is a command-line interface application for managing tasks, built with Node.js. The application allows users to view, add, delete, and update tasks in a MySQL database. It provides various options to handle tasks with different statuses like "Done", "Pending", and "To do".
+
+## Features
+
+1. **View All Tasks:** Displays a list of all tasks with their status.
+2. **Add a New Task:** Allows users to add a new task to the database.
+3. **Delete a Task:** Enables users to remove a task based on its index.
+4. **Mark a Task as Done:** Marks a selected task as "Done".
+5. **Mark a Task as Pending:** Marks a selected task as "Pending".
+6. **Mark a Task as To Do:** Marks a selected task as "To do".
+7. **Show Tasks Left to Do:** Displays a filtered list of tasks that are not yet completed.
+
+## Installation
+
+**1. Clone the repository to your local machine:**
+
+```bash
+git clone <repository-url>
+cd into the folder
+```
+
+**2. Install the dependencies:**
+
+```bash
+npm i
+```
+
+1. Modify the database configuration (`host`, `user`, `password`) as needed in the `database.js` file. The programm will create a database called 'task_manager' by itself when you run it.
+Be aware that you need a running local server in order to node to create the database. I personnaly use Docker and Stackbricks. 
+
+## Usage
+
+**1. Start the application**
+
+```bash
+node index.js
+```
+
+**2. Follow the prompts displayed in the terminal to manage your tasks:**
+
+```
+-------------------------------
+Welcome to your task manager, Press:
+1. to see all your tasks,
+2. to add a task,
+3. to delete a task,
+4. to mark a task as done,
+5. to mark a task as pending,
+6. to mark a task as to do,
+7. to show tasks left to do,
+8. to Exit the task manager
+-------------------------------
+```
+
+Enter the corresponding number for the action you want to perform.
+
---
+++ index.js
@@ -0,0 +1,256 @@
+const readline = require("readline");
+const rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout,
+});
+const { checkDatabase, db } = require("./database");
+
+const welcome_message = `
+-------------------------------
+Welcome to your task manager, Press:
+1. to see all your tasks,
+2. to add a task,
+3. to delete a task,
+4. to mark a task as done,
+5. to mark a task as pending,
+6. to mark a task as to do,
+7. to show tasks left to do,
+8. to Exit the task manager
+-------------------------------`;
+
+async function main() {
+	try {
+		console.log("-------------------------------");
+		await checkDatabase();
+		console.log("-------------------------------");
+	} catch (err) {
+		console.error(err);
+	} finally {
+		mainMenu();
+	}
+}
+
+function getAllTasks() {
+	return new Promise((resolve, reject) => {
+		db.query("SELECT * FROM tasks", (err, result) => {
+			if (err) {
+				reject(err);
+			} else {
+				resolve(result);
+			}
+		});
+	});
+}
+
+async function mainMenu() {
+	console.log(welcome_message);
+
+	rl.question("Your choice: ", (answer) => {
+		answer = parseInt(answer);
+		handleUserChoice(answer);
+	});
+}
+
+async function handleUserChoice(option) {
+	switch (option) {
+		case 1:
+			await display_tasks();
+			mainMenu();
+			break;
+
+		case 2:
+			add_task();
+			break;
+
+		case 3:
+			delete_task();
+			break;
+
+		case 4:
+			task_done();
+			break;
+
+		case 5:
+			task_pending();
+			break;
+
+		case 6:
+			task_todo();
+			break;
+
+		case 7:
+			await filter_task();
+			break;
+
+		case 8:
+			process.exit(0);
+			break;
+
+		default:
+			invalid_answer();
+			mainMenu();
+			break;
+	}
+}
+
+async function display_tasks() {
+	const tasks = await getAllTasks();
+
+	let index = 1;
+	console.log("-------------------------------");
+	tasks.forEach((task) => {
+		console.log(`${index}. ${task.task} | Status: ${task.status}`);
+		index++;
+	});
+}
+
+async function add_task() {
+	rl.question("Enter your task: ", async (answer) => {
+		try {
+			await new Promise((resolve, reject) => {
+				db.query(`INSERT INTO tasks (task) VALUES (?)`, [answer], (err) => {
+					if (err) {
+						reject(new Error(`Error adding task: ${err.message}`));
+					} else {
+						console.log("Task added");
+						resolve();
+					}
+				});
+			});
+		} catch (err) {
+			console.error("Failed to add task:", err.message);
+		} finally {
+			mainMenu();
+		}
+	});
+}
+
+async function delete_task() {
+	const tasks = await getAllTasks();
+	await display_tasks();
+
+	rl.question("Enter the number of the task to delete: ", (answer) => {
+		const taskIndex = parseInt(answer) - 1;
+		if (isNaN(taskIndex) || taskIndex < 0 || taskIndex >= tasks.length) {
+			console.log("Invalid index. Please select a valid index.");
+			delete_task();
+			return;
+		}
+		const taskIdToDelete = tasks[taskIndex].id;
+
+		db.query("DELETE FROM tasks WHERE id = ?", [taskIdToDelete], (err, result) => {
+			if (err) {
+				console.error("Error deleting task:", err.message);
+			}
+			if (result.affectedRows === 0) {
+				console.log("No task found with the given ID.");
+				delete_task();
+			}
+
+			console.log("Task deleted successfully.");
+			mainMenu();
+		});
+	});
+}
+
+async function task_done() {
+	const tasks = await getAllTasks();
+	await display_tasks();
+
+	rl.question("Enter the number of the task to mark as done: ", (answer) => {
+		const taskIndex = parseInt(answer) - 1;
+		if (isNaN(taskIndex) || taskIndex < 0 || taskIndex >= tasks.length) {
+			console.log("Invalid index. Please select a valid index.");
+			return task_done()
+
+		}
+		const taskIdToMark = tasks[taskIndex].id;
+		db.query("UPDATE tasks SET status = ? WHERE id = ?", ["Done", taskIdToMark], (err, result) => {
+			if (err) {
+				console.error("Error marking task as done:", err.message);
+			}
+			if (result.affectedRows === 0) {
+				console.log("No task found with the given ID.");
+				return task_done();
+			}
+
+			console.log("Task marked as done.");
+			mainMenu();
+		});
+	});
+}
+
+async function task_pending() {
+	const tasks = await getAllTasks();
+	await display_tasks();
+
+	rl.question("Enter the number of the task to mark as pending: ", (answer) => {
+		const taskIndex = parseInt(answer) - 1;
+		if (isNaN(taskIndex) || taskIndex < 0 || taskIndex >= tasks.length) {
+			console.log("Invalid index. Please select a valid index.");
+			return task_pending();
+		}
+		const taskIdToMark = tasks[taskIndex].id;
+		db.query("UPDATE tasks SET status = ? WHERE id = ?", ["Pending", taskIdToMark], (err, result) => {
+			if (err) {
+				console.error("Error marking task as Pending:", err.message);
+			}
+			if (result.affectedRows === 0) {
+				console.log("No task found with the given Index.");
+				return task_pending();
+
+			}
+
+			console.log("Task marked as Pending.");
+			mainMenu();
+		});
+	});
+}
+
+async function task_todo() {
+	const tasks = await getAllTasks();
+	await display_tasks();
+
+	rl.question("Enter the number of the task to mark as To do: ", async (answer) => {
+		const taskIndex = parseInt(answer) - 1;
+		if (isNaN(taskIndex) || taskIndex < 0 || taskIndex >= tasks.length) {
+			console.log("Invalid index. Please select a valid index.");
+			return task_todo();
+		}
+		const taskIdToMark = tasks[taskIndex].id;
+		db.query("UPDATE tasks SET status = ? WHERE id = ?", ["To do", taskIdToMark], (err, result) => {
+			if (err) {
+				console.error("Error marking task as To do:", err.message);
+			} 
+			if (result.affectedRows === 0) {
+				console.log("No task found with the given Index.");
+			} 
+			
+			console.log("Task marked as To do.");
+			mainMenu();
+		});
+	});
+}
+
+async function filter_task() {
+	try {
+		let filteredTasks = await getAllTasks();
+		filteredTasks = filteredTasks.filter(task => task.status === "To do");
+		let index = 1;
+		
+		filteredTasks.forEach((task) => {
+			console.log(`${index}. ${task.task}`);
+			index++;
+		});
+	} catch (err) {
+		console.error("Error filtering tasks:", err.message);
+	} finally {
+		mainMenu();
+	}
+}
+
+function invalid_answer() {
+	return console.log("Not a valid answer");
+}
+
+main();
